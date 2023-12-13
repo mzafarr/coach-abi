a55e5eface93da4a240215fea6e01def4f09f20f
Muhammad Zafar <mzafar611@gmail.com>
2023-12-13 22:27:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const app = express();
+const port = 3000; 
+
+// Middleware to parse JSON in the request body
+app.use(express.json());
+
+// Define a route for the API
+app.post('/api/chat', async (req, res) => {
+  try {
+    const messages = req.body.messages || [];
+    // Add a system message if not present
+    // if (!messages.some(msg => msg.role === 'system')) {
+    //   messages.unshift({ role: 'system', content: 'You are a helpful assistant.' });
+    // }
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+    });
+
+    res.json({ response: completion.choices[0].message.content });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
