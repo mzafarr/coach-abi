d20034d98fe14d456fb0d4ef8f8839bbc7a089c1
Muhammad Zafar <mzafar611@gmail.com>
2024-01-03 14:51:17 +0500

    commit

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-import express from 'express';
-import OpenAI from 'openai';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
 app.use(cors());
 // Define a route for the API
-app.post('/api/chat', async (req, res) => {
+app.post("/api/chat", async (req, res) => {
   try {
     const messages = req.body.messages || [];
-    messages.unshift({ role: 'system', content: `
-    You are an enthusiastic AI assistant whose name is CoachAbi, when user's messages that are in any way related to Neuro-Linguistic Programming then you must answer as if you're an expert Neuro-Linguistic Programming (NLP) practitioner. Try to answer their question or solve their issue in best possible way. Use positive language patterns. Keep your responses concise, unless the user requests detailed information or the context requires a more extended reply.
-    ` });
+    messages.unshift({
+      role: "system",
+      content: `
+    You are an AI assistant whose name is CoachAbi, when user's messages are in any way related to Life Coaching, Counselling, psychology or any talking therapy then you must answer as if you're an expert. Try to answer their question or solve their issue in best possible way. Use positive language patterns. Provide specific tools and techniques to help someone overcome a problem. Use tools and techniques from Neuro-linguistic programming, CBT, coaching, counselling, and other talking therapies and offer multiple examples of ways to overcome the problems.`,
+    });
     // Add a system message if not present
     // if (!messages.some(msg => msg.role === 'system')) {
     //   messages.unshift({ role: 'system', content: 'You are a helpful assistant.' });
     // }
     const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: "gpt-3.5-turbo",
       max_tokens: 1500,
       messages: messages,
     });
     res.json({ response: completion.choices[0].message.content });
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
